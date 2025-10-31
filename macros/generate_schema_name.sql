36e8be4c6fdd04b6aa25888d3fee6429b1bb01f0
sagara.satoshi@classmethod.jp <sagara.satoshi@classmethod.jp>
2025-10-31 07:01:59 +0000

    generate_schema_nameマクロの変更

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,21 +1,24 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
 
     {% set default_schema = target.schema %}
-
-    {# seeds go in a global `raw` schema #}
-    {% if node.resource_type == 'seed' %}
+    
+    {# targetが「prod」かつ対象のオブジェクトが「seed」かつcustom_schemaの定義ありの場合、seed向けの「custom_schema」に #}
+    {% if target.name == 'prod' and node.resource_type == 'seed' and custom_schema_name is not none %}
         {{ custom_schema_name | trim }}
 
-    {# non-specified schemas go to the default target schema #}
+    {# targetが「prod」でないかつ対象のオブジェクトが「seed」かつcustom_schemaの定義ありの場合、「default_schema」を先頭に付けたseed向けの「custom_schema」に #}
+    {% elif target.name != 'prod' and node.resource_type == 'seed' and custom_schema_name is not none %}
+        {{ default_schema }}_{{ custom_schema_name | trim }}
+
+    {# targetが「prod」かつcustom_schemaの定義ありの場合、「custom_schema」に #}
+    {% elif target.name == 'prod' and custom_schema_name is not none %}
+        {{ custom_schema_name | trim }}
+
+    {# custom_schemaの定義なしの場合、「default_schema」に #}
     {% elif custom_schema_name is none %}
         {{ default_schema }}
 
-
-    {# specified custom schema names go to the schema name prepended with the the default schema name in prod (as this is an example project we want the schemas clearly labeled) #}
-    {% elif target.name == 'prod' %}
-        {{ default_schema }}_{{ custom_schema_name | trim }}
-
-    {# specified custom schemas go to the default target schema for non-prod targets #}
+    {# 上述の条件に合致しない（targetが「prod」でないがcustom_schemaの定義あり、など）の場合、「default_schema」に #}
     {% else %}
         {{ default_schema }}
     {% endif %}
